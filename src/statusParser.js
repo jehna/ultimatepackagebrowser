bfc817e00834b82d79423a3a097570a21b481dfc
Jaakko Jäkärä <jaakko.jakara@gmail.com>
2020-01-02 22:59:23 +0200

    Finalized 1st working version of status file parser

--- src/statusParser.js
+++ src/statusParser.js
@@ -1,8 +1,5 @@
-const readline = require("readline");
 const fs = require("fs");
-const sleep = require("thread-sleep");
 let useMockup = true;
-
 
 function parseStatusFile() {
   let filePath = useMockup ? "./status.mockup" : "/var/lib/dpkg/status";
@@ -12,22 +9,58 @@ function parseStatusFile() {
   let fileIterator = fs
     .readFileSync(filePath)
     .toString()
-    .split("\n")
+    .split("\r\n")
     [Symbol.iterator]();
 
-  // keep track of current line and next line (for multiline statements)
-  let [line, finished] = fileIterator.next();
-  let [nextLine, tmp] = fileIterator.next();
-  let current = {};
+  // keep track of current line and next line (for multiline values)
+  let line = fileIterator.next();
+  let nextLine = fileIterator.next();
+  let current = { name: "", description: "", dependencies: [], forwardDependencies: [] };
 
   // function to process all lines one step forward
   function stepIterator() {
     line = nextLine;
-    finished = tmp;
-    [nextLine, tmp] = fileIterator.next();
+    nextLine = fileIterator.next();
   }
 
+  while (!line.done) {
+    let completeValue = line.value;
 
+    // Construct multiline values correctly
+    while (!nextLine.done && nextLine.value[0] == " ") {
+      stepIterator();
+      completeValue += "\n";
+      completeValue += line.value;
+    }
+
+    if (completeValue.startsWith("Package: ")) {
+      current.name = completeValue.slice(9);
+    } else if (completeValue.startsWith("Description: ")) {
+      current.description = completeValue.slice(13);
+    } else if (completeValue.startsWith("Depends: ")) {
+      current.dependencies = current.dependencies.concat(
+        completeValue
+          .slice(9)
+          .split(",")
+          .map(x => x.split(" ").filter(x => x !== "")[0]),
+      );
+    } else if (completeValue === "") {
+      // Empty line means end of current package, save result and initialize next one
+      packages.push(current);
+      current = current = { name: "", description: "", dependencies: [], forwardDependencies: [] };
+    }
+
+    stepIterator();
+  }
+
+  // make a second pass to add forwardDependencies
+  packages.forEach(pac => {
+    pac.dependencies.forEach(dep => {
+      // Not all dependencies are necessarily installed so have to check for it
+      let dependency = packages.find(x => x.name === dep);
+      if (dependency != undefined) dependency.forwardDependencies.push(pac.name);
+    });
+  });
+
+  return packages;
 }
-
-parseStatusFile();
